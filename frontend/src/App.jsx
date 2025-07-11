b029c4136ef002166b6c2012b2dfd5aeff606d79
TakashufShakoor <takashufshakoor1@gmail.com>
2025-07-12 02:52:28 +0500

    Project Completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { ToastContainer, toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+
 
 const API_URL = "http://localhost:4000/shorten";
 
